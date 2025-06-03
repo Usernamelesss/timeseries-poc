7d61fb9c3b3bdf4714a4a20d59adfbe841fc1bf4
Usernamelesss <8vd2roma@gmail.com>
2025-06-03 15:20:14 +0200

    write timing into csv

--- python-poc/src/timeseries/main.py
+++ python-poc/src/timeseries/main.py
@@ -26,13 +26,13 @@ def exponential_moving_average(df: pd.DataFrame) -> pd.DataFrame:
     return df.ewm(span=10, adjust=False, min_periods=10).mean()
 
 
-def bench(description, fn, *args, **kwargs) -> pd.DataFrame:
+def bench(description, fn, *args, **kwargs) -> (pd.DataFrame, float):
     start = time.monotonic_ns()
     r = fn(*args, **kwargs)
     end = time.monotonic_ns()
     elapsed = (end - start) / 1e+6
     print("{} took {:0.2f} ms".format(description, elapsed))
-    return r
+    return r, elapsed
 
 
 if __name__ == '__main__':
@@ -41,11 +41,22 @@ if __name__ == '__main__':
 
     os.makedirs(output_dir, exist_ok=True)
 
-    r1 = bench("Divide By 2", divide_by_2, timeseries)
+    r1, elapsed1 = bench("Divide By 2", divide_by_2, timeseries)
     r1.to_parquet(os.path.join(output_dir, "python_divide_by2.parquet"))
 
-    r3 = bench("Sqrt", vectorized_sqrt_formula, timeseries)
+    r3, elapsed2 = bench("Sqrt", vectorized_sqrt_formula, timeseries)
     r3.to_parquet(os.path.join(output_dir, "python_sqrt.parquet"))
 
-    r4 = bench("EWMA", exponential_moving_average, timeseries)
+    r4, elapsed3 = bench("EWMA", exponential_moving_average, timeseries)
     r4.to_parquet(os.path.join(output_dir, "python_ema.parquet"))
+
+    timing_path = os.path.join(output_dir, "python_timing.csv")
+    timing_df = pd.DataFrame({
+            "divide_by_2[ms]": [elapsed1],
+            "sqrt[ms]": [elapsed2],
+            "ewma[ms]": [elapsed3],
+        })
+    if os.path.exists(timing_path):
+        timing_df.to_csv(timing_path, mode="a", index=False, header=False)
+    else:
+        timing_df.to_csv(os.path.join(output_dir, "python_timing.csv"), mode="x", index=False)
