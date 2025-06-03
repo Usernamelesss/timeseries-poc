1a9a574051639d1e0844199e28592ece8238d4cc
Usernamelesss <8vd2roma@gmail.com>
2025-06-03 15:18:37 +0200

    Write timing into csv

--- go-poc/src/main.go
+++ go-poc/src/main.go
@@ -14,23 +14,25 @@ func main() {
 
 	matrix := data.ToMatrix()
 
-	r1 := utils.Bench("Divide by 2", func() [][]float64 {
+	r1, elapsed1 := utils.Bench("Divide By 2", func() [][]float64 {
 		return utils.DivideBy2(matrix)
 	})
-	r2 := utils.Bench("Sqrt", func() [][]float64 {
+	r2, elapsed2 := utils.Bench("Sqrt", func() [][]float64 {
 		return utils.Sqrt(matrix)
 	})
-	utils.Bench("Divide by 2 chunking", func() [][]float64 {
-		return utils.DivideBy2Chunking(matrix)
-	})
-	utils.Bench("Sqrt chunking", func() [][]float64 {
-		return utils.SqrtChunking(matrix)
-	})
-	r3 := utils.Bench("Exponential Moving Average", func() [][]float64 {
+	//utils.Bench("Divide by 2 chunking", func() [][]float64 {
+	//	return utils.DivideBy2Chunking(matrix)
+	//})
+	//utils.Bench("Sqrt chunking", func() [][]float64 {
+	//	return utils.SqrtChunking(matrix)
+	//})
+	r3, elapsed3 := utils.Bench("EWMA", func() [][]float64 {
 		return ewma.ProcessDataFrame(matrix, 10, false, 10)
 	})
 
 	utils.WriteTimeseries("golang_divide_by2.parquet", utils.FromMatrix(r1, data.GetIndex()))
 	utils.WriteTimeseries("golang_sqrt.parquet", utils.FromMatrix(r2, data.GetIndex()))
 	utils.WriteTimeseries("golang_ema.parquet", utils.FromMatrix(r3, data.GetIndex()))
+
+	utils.WriteTimings(elapsed1, elapsed2, elapsed3)
 }
--- go-poc/src/utils/bench.go
+++ go-poc/src/utils/bench.go
@@ -1,13 +1,54 @@
 package utils
 
 import (
+	"encoding/csv"
 	"fmt"
+	"os"
+	"strconv"
 	"time"
 )
 
-func Bench(name string, function func() [][]float64) [][]float64 {
+func Bench(name string, function func() [][]float64) ([][]float64, int64) {
 	start := time.Now()
 	result := function()
-	fmt.Println(fmt.Sprintf("%s took %v", name, time.Since(start)))
-	return result
+	elapsed := time.Since(start)
+	fmt.Println(fmt.Sprintf("%s took %v", name, elapsed))
+	return result, elapsed.Milliseconds()
+}
+
+func WriteTimings(elapsed ...int64) {
+	pathToFile := fmt.Sprintf("%s/results/golang_timing.csv", os.Getenv("PROJECT_ROOT"))
+
+	fileExists := false
+	if _, err := os.Stat(pathToFile); err == nil {
+		fileExists = true
+	}
+
+	file, err := os.OpenFile(pathToFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, os.ModePerm)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	if !fileExists {
+		if err = writer.Write([]string{"divide_by_2[ms]", "sqrt[ms]", "ewma[ms]"}); err != nil {
+			fmt.Println("Error writing headers:", err)
+			return
+		}
+	}
+
+	err = writer.Write([]string{
+		strconv.FormatInt(elapsed[0], 10),
+		strconv.FormatInt(elapsed[1], 10),
+		strconv.FormatInt(elapsed[2], 10),
+	})
+	if err != nil {
+		panic(fmt.Sprintf("Failed to write data: %v", err))
+	}
+
+	writer.Flush()
 }
